4f3996ca742ea293c3bd60926a2e46e9c1e98842
Chris Barts <chbarts@gmail.com>
2025-02-06 18:29:43 -0700

    Fix many bugs.

--- atom-to-html.py
+++ atom-to-html.py
@@ -8,7 +8,7 @@ from airium import Airium
 from datetime import datetime, timezone
 
 def utc_to_local(utc_dt):
-    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
+    return utc_dt.now().astimezone()
 
 def make_html(feed):
     air = Airium()
@@ -19,13 +19,13 @@ def make_html(feed):
     with air.html(lang=lang):
         with air.head():
             air.meta(charset="utf-8")
-            air.title(_t=feed.title)
+            air.title(_t=feed.title.value)
             if hasattr(feed, 'link'):
                 air.link(href=feed.link, type="application/atom+xml", rel="alternate", title=feed.title)
         with air.body():
             if hasattr(feed, 'link'):
                 with air.p(klass="link"):
-                    air.a(href=feed.link, _t=feed.title)
+                    air.a(href=feed.link, _t=feed.title.value)
             with air.div(klass="main"):
                 if hasattr(feed, 'description'):
                     with air.p(klass="description", style="background-color: rgb(220,220,220); margin: 1em;"):
@@ -36,37 +36,38 @@ def make_html(feed):
                     with air.div(klass="item", style="border: 1px solid white; margin: 1em; background-color: rgb(231,254,255);"):
                         with air.p():
                             air.strong(_t=item.title.value)
-                            if hasattr(item, 'published'):
+                            if hasattr(item, 'published') and item.published:
                                 air(': ')
                                 date = utc_to_local(item.published)
                                 air(date.strftime("%A, %B %d, %Y %T %z"))
-                            if hasattr(item, 'updated'):
+                            if hasattr(item, 'updated') and item.updated:
                                 date = utc_to_local(item.updated)
                                 air(" Updated: {0}".format(date.strftime("%A, %B %d, %Y %T %z")))
                         with air.ol():
                             for author in item.authors:
                                 with air.li():
                                     air("Author: {0}".format(author.name))
-                                    if hasattr(author, 'email'):
+                                    if hasattr(author, 'email') and author.email:
                                         air.a(href="mailto:{0}".format(author.email), _t=" Email")
-                                    if hasattr(author, 'uri'):
+                                    if hasattr(author, 'uri') and author.uri:
                                         air.a(href=author.uri, _t=" Link")
                         with air.ol():
                             for contributor in item.contributors:
                                 with air.li():
                                     air("Contributor: {0}".format(contributor.name))
-                                    if hasattr(contributor, 'email'):
+                                    if hasattr(contributor, 'email') and contributor.email:
                                         air.a(href="mailto:{0}".format(contributor.email), _t=" Email")
-                                    if hasattr(contributor, 'uri'):
+                                    if hasattr(contributor, 'uri') and contributor.uri:
                                         air.a(href=contributor.uri, _t=" Link")
-                        with air.div(klass="itemdesc"):
-                            air(item.content.value)
+                        if hasattr(item, 'content') and item.content:
+                            with air.div(klass="itemdesc"):
+                                air(item.content.value)
                         with air.div(klass="links", style="background-color: rgba(0,128,64,.25);"):
                             with air.ol():
                                 for link in item.links:
                                     with air.li():
                                         title = "Link"
-                                        if hasattr(link, 'title'):
+                                        if hasattr(link, 'title') and link.title:
                                             title = link.title
                                         air.a(href=link.href, _t=title)
     return str(air)
